53677e487e998edf601113dfdd75ca37a54332c1
Wallsays <derevyankodenis92@gmail.com>
2013-06-30 02:38:28 +0700

    added validators to Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,10 @@
 class Item < ActiveRecord::Base
   attr_accessible :price, :name, :real, :weight, :description
 
-
-end
+  validates :price, { numericality: { greater_than: 0, allowed_nil: true} }
+  # same
+  #validates :price, numericality: { greater_than: 0, allowed_nil: true}
+  validates :name, :description. presence: true
+  
+  
+end 
