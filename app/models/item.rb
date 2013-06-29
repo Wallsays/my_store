2843c9bfd8e48c1e48c0052296e25b7a75a0d36b
Wallsays <derevyankodenis92@gmail.com>
2013-06-30 02:51:23 +0700

    callbacks in Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,24 @@
 class Item < ActiveRecord::Base
   attr_accessible :price, :name, :real, :weight, :description
 
-  validates :price, { numericality: { greater_than: 0, allowed_nil: true} }
+  validates :price, { numericality: { greater_than: 0, allow_nil: true} }
   # same
   #validates :price, numericality: { greater_than: 0, allowed_nil: true}
-  validates :name, :description. presence: true
+  validates :name, :description, presence: true
   
-  
+  belongs_to :category
+
+  after_initialize { puts "initialized" } # Item.new; Item.first
+  after_save       { puts "saved"    } # Item.save; Item.create; item.update_attributes
+  after_create     { puts "created" 
+  					 # ItemMailer.new_item_created(self).deliver 
+  					 # category.inc(:items_count, 1) # increment
+  					} 
+  after_update     { puts "updated"  } 
+  after_destroy    { puts "destroyed"
+  					 # category.inc(:items_count, -1) 
+  				   } # item.destoy
+
+
+
 end 
