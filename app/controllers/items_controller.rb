538ff696c0838d1f8945e79a43818f09c29068fe
Wallsays <derevyankodenis92@gmail.com>
2013-06-30 03:40:07 +0700

    params in controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,18 @@ class ItemsController < ApplicationController
 	# plain text rendering
     render text: @items.map { |i| "#{i.name}: #{i.price}"}.join("<br/>") 
   end
+
+  def create
+  	# same functionality except of format req
+  	# http://localhost:3000/items/create?description=good+car&price=500000&weight=0&real=1
+  	# http://localhost:3000/items/create?item[name]=car1&item[description]=good+car&item[price]=500000&item[weight]=0&item[real]=1
+ 	# i.e. hash in hash
+
+  	# @item = Item.create(name: params[:name], description: params[:description], price: params[:price], real: params[:real], weight: params[:weight])
+  	@item = Item.create(params[:item])
+
+  	# last checks if all validations is OK
+  	render text: "#{@item.id}: #{@item.name} (#{!@item.new_record?})"
+  end
   
 end
