bb3defa78841b9f8b9178db3a12bd03b20a555c8
Wallsays <derevyankodenis92@gmail.com>
2013-06-30 22:01:47 +0700

    adding Order model, association has_many - belongs_to

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   
-  attr_accessible :login
+  attr_accessible :login #, :email, :password, :password_confirmation, :remember_me
 
-  has_one :cart
-  
+  has_one  :cart
+  has_many :orders
+
 end
