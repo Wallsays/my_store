1e1712c831f75f85d072d22aed055e8fe7ef6285
Wallsays <derevyankodenis92@gmail.com>
2013-07-01 02:33:12 +0700

    adding helpers to view images by urls and clickable links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,15 @@ class ApplicationController < ActionController::Base
 
     # if not Admin i.e. permission denied
     def render_403
-  	  render file: "public/403.html", status: 403
+  	  render file: "public/403", formats: :html, status: 403
     end
   
     def render_404
-  	  render file: "public/404.html", status: 404
+  	  render file: "public/404", formats: :html, status: 404
     end
 
     def check_if_admin
-      render_403 unless params[:admin] 
+      # render_403 unless params[:admin] 
     end 
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  
+  def urls_to_images(s)
+  	# заменяет все совпадения на строку содержащуюся во втором аргументе
+  	s.gsub! /\s(http:\/\/.*?\.(png|jpeg|jpg))/, 
+  			'<p><img src="\1"/></p>'
+  	s.html_safe
+  end
+
+  def urls_to_links(s)
+  	s.gsub! /\s(http:\/\/.*)/, 
+  			'<a href="\1"/>\1</a>'
+  	s.html_safe
+  end
+  
 end
